ecb84d148748e4fab72f0d96c5a4c49ac6b70d5e
hduWendy <576939544@qq.com>
2020-03-27 14:36:37 +0800

    ok

--- 19_camera_Average_filtering/19_camera_Average_filtering.cpp
+++ 19_camera_Average_filtering/19_camera_Average_filtering.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
 
-
-int main()
+int main()  //均值滤波
 {
-    return 0;
+	VideoCapture cap(0);
+	double scale = 0.5;
+
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat hsvMat;
+		cv::Mat averageMat;
+
+		cap >> frame;
+		Size ResImgSiz = Size(frame.cols*scale, frame.rows*scale);
+		Mat rFrame = Mat(ResImgSiz, frame.type());
+		resize(frame, rFrame, ResImgSiz, INTER_LINEAR);
+
+		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
+
+		blur(rFrame, averageMat, Size(3, 3),Point(-1,-1));//均值滤波
+
+		cv::imshow("whie: in the Average filtering", averageMat);
+		cv::imshow("frame", rFrame);
+
+		waitKey(30);
+	}
 }
 
